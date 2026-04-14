c27109d1910c46029ae2ea1a7a50d3476997437c
XINICOM <XINICOM@outlook.com>
2026-04-14 23:39:55 +0800

    simple using

--- WinUINotes/App.xaml.cs
+++ WinUINotes/App.xaml.cs
@@ -1,20 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.Extensions.DependencyInjection;
 using WinUINotes.Bus.ViewModels;
 using WinUINotes.Bus.Services;
@@ -50,7 +35,7 @@ namespace WinUINotes
         //    _window.Activate();
         //}
 
-        private readonly IServiceProvider _serviceProvider;
+        //private readonly IServiceProvider _serviceProvider;
 
         public App()
         {
